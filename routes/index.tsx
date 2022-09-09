48b86f7179a829f9cd89ac8424cbc4e2d80aa067
minmin0530 <izumiyoshiki@icloud.com>
2022-09-09 11:39:40 +0900

    6th commit

--- routes/index.tsx
+++ routes/index.tsx
@@ -47,10 +47,10 @@ export default function Index() {
       <h1 className="text-3xl font-bold mt-2">
         The Fullstack Framework in Deno.
       </h1>
-      <h2>article[0].auth</h2>
-      <h2>article[0].title</h2>
-      <h2>article[0].article</h2>
-      <h2>article[0].date</h2>
+      <h2>{article[0].auth}</h2>
+      <h2>{article[0].title}</h2>
+      <h2>{article[0].article}</h2>
+      <h2>{article[0].date}</h2>
 
       <p className="text-center text-md text-gray-800">
         <strong>Aleph.js</strong> gives you the best developer experience for building web applications<br />{" "}
