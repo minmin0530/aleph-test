3d73d1a93843233ca2ddcba406e5634fefe7956b
minmin0530 <izumiyoshiki@icloud.com>
2022-09-09 11:43:46 +0900

    8th commit

--- routes/index.tsx
+++ routes/index.tsx
@@ -45,11 +45,9 @@ export default function Index() {
         <img src="./assets/logo.svg" width="75" height="75" title="Aleph.js" />
       </p>
       <h1 className="text-3xl font-bold mt-2">
-        The Fullstack Framework in Deno.
-      </h1>
-      <h2 className="text-3xl font-bold mt-2">
         The Fullstack Framework in Deno.{article[0].auth}
-      </h2>
+        1111
+      </h1>
       <h2>{article[0].auth}</h2>
       <h2>{article[0].title}</h2>
       <h2>{article[0].article}</h2>
